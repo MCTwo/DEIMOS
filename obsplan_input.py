719b9bd9334149ec138bf8f9d4e15b7bacacb842
Will Dawson <will@dawsonresearch.com>
2013-06-10 14:00:47 -0700

    major additions to obsplan_input.py file

--- obsplan_input.py
+++ obsplan_input.py
@@ -3,22 +3,35 @@ This file provides an example of how to interact and run obsplan.py
 
 All position angles are defined +CCW from North towards East
 '''
+from __future__ import division
+import tools
+import obsplan
 
 ###########################################################################
-## USER INPUT 
+## USER INPUT
 ###########################################################################
 
 ## General inputs
 
 #Prefix for all output files
 prefix = 'Mask1_rev0'
+# Hour angle of the target for the mask (float; unit:hours)
+HA = -40/60.
 
 ## Star and Galaxy catalog inputs
 
 catalog = 'catalog.txt'
+objid_ttype = 'objID'
 ra_ttype = 'ra'
 dec_ttype = 'dec'
 mag_ttype = 'dered_r'
+passband = 'R'
+equinox = '2000'
+
+# Go ahead and read in the catalog since this will be needed to create the
+# galaxy mask later in the user input section
+cat = tools.readcatalog(catalog)
+key = tools.readheader(catalog)
 
 ## Slitmask ds9 region input
 
@@ -54,10 +67,82 @@ as_ids = (646957174, 646957223, 646826017, 646826203, 646957144)
 
 ## Exclusion list input
 
-#List of preselected galaxies to exclude from mask (excludes matching ttypeX = obsid
-exfile = 'exfile_rev4.txt'
+# ttype catalog of galaxies to exclude from mask (excludes matching ttype = objid). exobjid_ttype is ['string'] ttype name of the objid column in the exfile, the objid's should correspond to some of the objid's in the objid array
+exfile = None #a string (e.g. 'exclusion.txt') or None
+exobjid_ttype = 'objid' 
 
 ## Preselected list input
-#Preselection list
-presel ='preselect_mask1_rev0.txt' #a string (e.g. 'preselect.txt') or None
 
+# ttype catalog of galaxies to preselect in dsim.
+psfile = None #a string (e.g. 'preselect.txt') or None
+psobjid_ttype = None
+
+## Create galaxy selection mask
+
+# Some how the user at this point needs to create a boolean type mask for the
+# catalog that will filter out all objects that are not galaxies
+mask_galaxy = cat[:,key['type']] == 3
+
+## Create a magnitude mask
+
+# It is likely that the a faint end mask should be used
+mask_mag = cat[:,key[mag_ttype]] <= 22.5
+
+## Create a sample definition
+
+
+###########################################################################
+## Automated Portion
+###########################################################################
+
+# create basic 1D arrays from catalog
+objid = cat[:,key[obid_ttype]]
+ra = cat[:,key[ra_ttype]]
+dec = cat[:,key[dec_ttype]]
+mag = cat[:,key[mag_ttype]]
+
+# Create the slitmask mask
+mask_slitmask = obsplan.createSlitmaskMask(regfile,ra,dec)
+
+# Create the exclusion list mask
+mask_ex = obsplan.createExclusionMask(objid,exfile,exobjid_ttype)
+
+# Determine the priority_code (i.e. weight) for each galaxy
+
+# Determine the sample for each galaxy (sample 1 objects selected first, then
+# sample 2, etc.). This order of selection take priority over the priority_code
+
+# Determine the selection flag for each galaxy. If non-zero then the object is
+# preselected
+selectflag = assignSelectionFlag(objid,psfile,psobjid_ttype)
+
+# determine object declination and the mask PA from the regfile
+box = obsplan.readMaskRegion(regfile)
+delta = box[1]
+pa_mask = box[4]
+
+# Determine the optimal slit PA
+pa_slit = obsplan.optimalPA(pa_mask,HA,delta)
+
+# Determine the slit size for each object
+len1, len2 = obsplan.slitsize(pa_slit,sky,A_gal,B_gal,pa_gal)
+
+# Create the output dsim file
+outcatname = prefix+'_maskcat.txt'
+print 'started to write out to ', outcatname
+F = open(outcatname,'w')
+
+# Write the dsim header information to the output file
+obsplan.write_dsim_header(F,regfile)
+
+# Write the guide star info to the dsim output file
+obsplan.write_guide_stars(F,gs_ids,objid,ra,dec,mag,equinox,passband)
+
+# Write the alignment star info to the dsim output file
+obsplan.write_guide_stars(F,as_ids,objid,ra,dec,mag,equinox,passband)
+
+# Filter the galaxy catalog before creating dsim input
+mask = mask_galaxy*mask_mag*mask_ex*mask_slitmask
+
+# Write the galaxy info to the desim output file
+write_galaxies_to_dsim(F,objid[mask],ra[mask],dec[mask],mag[mask],priority_code[mask],sample[mask],selectflag[mask],pa_slit[mask],len1[mask],len2[mask],equinox='2000',passband='R')
